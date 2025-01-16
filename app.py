d6cf94097ab3be06ec78ee4896453be999a3ebbf
Bachtiar <121807988+chidorisasuke@users.noreply.github.com>
2025-01-16 23:20:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,20 +4,20 @@ import streamlit as st
 st.set_page_config(page_title="ALus", page_icon=":lungs:", layout="wide", initial_sidebar_state="collapsed")
 
 from home import display_home
-from diagnose import display_diagnosis
+from model_deploy import display_diagnosis
 from about import display_about
-from chatbot import display_chatbot
+# from chatbot import display_chatbot
 
 # Navbar dengan radio button di bagian atas
-menu = ["Beranda", "Konsultasi", "Diagnosa", "Tentang"]
+menu = ["Beranda", "Diagnosa", "Tentang"]
 choice = st.radio("Navigasi", menu, horizontal=True)
 
 # Tampilkan halaman sesuai pilihan dari radio button
 if choice == "Beranda":
     display_home()
 
-elif choice == "Konsultasi":
-    display_chatbot()
+# elif choice == "Konsultasi":
+    # display_chatbot()
 
 elif choice == "Diagnosa":
     display_diagnosis()
